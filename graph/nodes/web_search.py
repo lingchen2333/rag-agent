1d9371c7ef9ae9d7500c6083ef0f120d90b70aa8
lingchen <138715540+lingchen2333@users.noreply.github.com>
2025-10-28 22:03:15 +0000

    add web_search node

---
+++ graph/nodes/web_search.py
@@ -0,0 +1,32 @@
+from typing import Any, Dict
+
+from dotenv import load_dotenv
+from langchain_core.documents import Document
+from langchain_tavily import TavilySearch
+
+from graph.state import GraphState
+
+load_dotenv()
+
+web_search_tool = TavilySearch(max_results = 3)
+
+def web_search(state: GraphState) -> Dict[str, Any]:
+    print("---web-search---")
+    question = state["question"]
+    documents = state["documents"]
+
+    tavily_result = web_search_tool.invoke({"query": question})
+    joined_tavily_result = "\n".join(
+       [res["content"] for res in tavily_result["results"]]
+    )
+
+    web_result = Document(page_content=joined_tavily_result)
+    if documents is not None:
+        documents.append(web_result)
+    else:
+        documents = [web_result]
+
+    return {"question": question, "documents": documents}
+
+if __name__ == "__main__":
+    web_search(state ={"question": "agent memory", "documents": None})
