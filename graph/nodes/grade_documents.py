96cd2a7ed429ab31f68d86149811712129f811be
lingchen <138715540+lingchen2333@users.noreply.github.com>
2025-10-28 21:30:16 +0000

    add grade_documents node

---
+++ graph/nodes/grade_documents.py
@@ -0,0 +1,35 @@
+from graph.chains import retriever_grader
+from graph.state import GraphState
+
+
+def grade_documents(state: GraphState):
+    """
+    Determine whether the retrieved documents are relevant to the question.
+    If any document is not relevant, we will set a flag to run web search
+
+    args:
+        state (dict): The current graph state
+    return:
+        state (dict): filtered out irrelevant documents and update web_search state
+    """
+
+    print("---checking document relevance---")
+    question = state["question"]
+    documents = state["documents"]
+
+    filtered_documents = []
+    web_search = False
+
+    for document in documents:
+        score = retriever_grader.invoke(
+            {"question": question, "document": document.page_content}
+        )
+        grade = score.binary_score
+        if grade.lower() == "yes":
+            print("---grade: document relevant---")
+            filtered_documents.append(document)
+        else:
+            print("---grade: document not relevant---")
+            web_search = True
+
+    return {"documents": filtered_documents, "question": question, "web_search": web_search}
